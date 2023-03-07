f162cc9bd0bd65b6495ace879212e22f88e1cb70
Sridhar D Kedlaya <sridhardked@gmail.com>
2023-03-07 19:53:05 +0530

    Fix api-call partially

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-$(document).ready(function () {
-  $("#generate-button").click(function () {
-    $.ajax({
-      url: "https://api.openai.com/v1/engines/text-davinci-002/jobs",
-      type: "POST",
-      headers: {
-        Authorization:
-          "Bearer <API GOES HERE>",
-      },
-      data: JSON.stringify({
-        prompt: $("#prompt").val() + " " + $("#tone").val(),
-        max_tokens: $("#word-count").val(),
-      }),
-      contentType: "application/json",
-      success: function (response) {
-        $("#generated-essay").val(response.choices[0].text);
-      },
-      error: function (error) {
-        console.log(error);
-      },
-    });
-  });
-});
+function generate() {
+
+  var prompt_text = document.getElementById('essay-topic').value + document.getElementById('tone').value
+  var url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url);
+
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.setRequestHeader("Authorization", "Bearer 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");
+  xhr.onreadystatechange = function () {
+     if (xhr.readyState === 4) {
+        open_ai_response = xhr.responseText;
+        console.log(xhr.status);
+        console.log(open_ai_response);
+	     response();
+     }};
+
+  var data = `{
+    "prompt": "${prompt_text}",
+    "temperature": 0.7,
+    "max_tokens": 256,
+    "top_p": 1,
+    "frequency_penalty": 0.75,
+    "presence_penalty": 0
+  }`;
+
+  xhr.send(data);
+}
+
+function response(){
+	document.getElementById('generated-essay').innerHTML = open_ai_response;
+}
